f0e9f3f488caef7dac89eb4eaeac4341b444ad95
Vyacheslav Mikushev <vyacheslav.mik.8@gmail.com>
2017-12-28 16:35:03 +0300

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject css-anal-tmp "0.1.0-SNAPSHOT"
+(defproject css-anal "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot css-anal-tmp.core
+  :main ^:skip-aot css-anal.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
