31a6c1872c0536e01fa90ce528b418024b1cb0c4
Vyacheslav Mikushev <vyacheslav.mik.8@gmail.com>
2017-12-28 13:51:56 +0300

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to css-anal-tmp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/css_anal_tmp/core_test.clj
@@ -0,0 +1,7 @@
+(ns css-anal-tmp.core-test
+  (:require [clojure.test :refer :all]
+            [css-anal-tmp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
